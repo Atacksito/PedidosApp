b3c5ed9fe3dc2e7b71e86b59c88bb3937b67b32f
Atacksito <165201064+Atacksito@users.noreply.github.com>
2025-04-24 15:31:16 -0500

    feat: Agregar clase Pedido

--- PedidosApp/Pedido.cs
+++ PedidosApp/Pedido.cs
@@ -8,6 +8,7 @@ namespace PedidosApp
 {
     public class Pedido
     {
+
         public string Cliente { get; set; }
         public string Producto { get; set; }
         public bool Urgente { get; set; }
